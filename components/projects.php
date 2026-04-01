43e3389fc10cd0478398789c8e7e73a56f6e058f
Lêvy Alves <levy.pereiraA1532@gmail.com>
2026-04-01 17:49:49 -0300

    Construindo seção de projetos

---
+++ components/projects.php
@@ -0,0 +1,16 @@
+<div class="p-[12px] rounded-[12px] bg-[#292C34] flex gap-[16px]">
+    <div class="w-2/5">
+        <img src="./assets/images/project1.png" alt="" class="">
+    </div>
+
+    <div class="flex-1 flex flex-col">
+        <div class="flex-1">
+            <p class="text-[16px] font-maven text-white font-bold mb-[8px]">Travelgram</p>
+            <p class="text-[14px] text-gray-200">Rede social onde as pessoas mostram os registros de suas viagens pelo mundo</p>
+        </div>
+
+        <div class="flex gap-[8px]">
+            <span class="py-[2px] px-[8px] text-[12px] rounded-full bg-indigo-400 font-inconsolata font-medium uppercase">PHP</span>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -37,26 +37,15 @@
             <?php require('./components/hero.php'); ?>
 
             <!-- Projects Section -->
-            <section class="">
-                <p class=""></p>
-                <h2 class=""></h2>
+            <section class="pt-[72px] pb-[144px]">
+                <div class="max-w-[1040px] w-full mx-auto px-5">
+                    <div class="text-center mb-[56px]">
+                        <p class="text-[20px] font-inconsolata text-red-300 mb-[8px]">Meu trabalho</p>
+                        <h2 class="text-[24px] font-maven text-white font-bold">Veja os projetos em destaque</h2>
+                    </div>
 
-                <div class="">
-                    <div class="">
-                        <div class="">
-                            <img src="" alt="" class="">
-                        </div>
-
-                        <div class="">
-                            <div class="">
-                                <p class=""></p>
-                                <p class=""></p>
-                            </div>
-
-                            <div class="">
-                                <span class=""></span>
-                            </div>
-                        </div>
+                    <div class="grid grid-cols-2 gap-[24px]">
+                        <?php require('./components/projects.php'); ?>
                     </div>
                 </div>
             </section>
