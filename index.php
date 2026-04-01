339e48cd6d307e01d86bc144afc7905e71b498ea
Lêvy Alves <levy.pereiraA1532@gmail.com>
2026-04-01 11:32:38 -0300

    Criando a estrutura inicial do projeto

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Desafio Portfolio Dev</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Asap:ital,wght@0,100..900;1,100..900&family=Inconsolata:wght@200..900&family=Maven+Pro:wght@400..900&display=swap" rel="stylesheet">
+
+        <!-- Tailwind CSS -->
+         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    </head>
+    <body>
+        <main class="">
+            <!-- Hero Section -->
+            <section class="">
+                <div class="">
+                    <img src="" alt="" class="">
+
+                    <p class=""></p>
+                    <h1 class=""></h1>
+
+                    <p class=""></p>
+
+                    <div class="">
+                        <span class=""></span>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Projects Section -->
+            <section class="">
+                <p class=""></p>
+                <h2 class=""></h2>
+
+                <div class="">
+                    <div class="">
+                        <div class="">
+                            <img src="" alt="" class="">
+                        </div>
+
+                        <div class="">
+                            <div class="">
+                                <p class=""></p>
+                                <p class=""></p>
+                            </div>
+
+                            <div class="">
+                                <span class=""></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Social Links Section -->
+            <section class="">
+                <p class=""></p>
+                <h3 class=""></h3>
+                <p class=""></p>
+
+                <div class="">
+                    <a href="#" class="">
+                        <img src="" alt="" class="">
+                        <p class=""></p>
+                        <img src="" alt="" class="">
+                    </a>
+                </div>
+            </section>
+        </main>
+    </body>
+</html>
